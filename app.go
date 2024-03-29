2e2b18c5fe944da226355c0f577041cc07747a5a
Renan G Dutra <r.granatodutra@gmail.com>
2024-03-29 01:57:39 -0300

    feat: add app main menu

--- app.go
+++ app.go
@@ -1,10 +1,32 @@
 package main
 
-import "go-json-notes/notation"
+import (
+	"fmt"
+	"go-json-notes/utils"
+)
 
-func main() {
-	nt := notation.New("Teste", "Olá mundo!")
-	nt.ChangeTitle("Hello World")
-	nt.ChangeText("This is my notation... the world is fantastic!")
-	nt.Print()
+const notesPath string = "C:\\Users\\rgran\\OneDrive\\Área de Trabalho\\notes"
+const notesExt string = ".jn"
+
+func startApp() {
+	for {
+		fmt.Println("Main menu:")
+		fmt.Println("1 - Create a new note")
+		fmt.Println("2 - List notes")
+		fmt.Println("0 - Exit program")
+
+		chosenOption := utils.GetUserInput("Enter your choice: ")
+
+		switch chosenOption {
+		case "1":
+			//createNewNote()
+		case "2":
+			//listSavedNotes()
+		case "0":
+			fmt.Println("Exiting program...")
+			return
+		default:
+			fmt.Println("Invalid option. Please try again.")
+		}
+	}
 }
